5244786b6f460b8501cca703fc96f147776d8267
calvean <garnetsharara@gmail.com>
2023-04-18 22:29:40 +0200

    changed let to const

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr = [];
-  for (let idx of array) {
-    let value = idx;
+  for (const idx of array) {
+    const value = idx;
     arr.push(appendString + value);
   }
 
