50f2a35cbdbdd7bdbbd17409290fded7cda07944
calvean <garnetsharara@gmail.com>
2023-04-19 22:23:59 +0200

    fixed eslint

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,14 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
+    uploadPhoto(fileName),
   ];
 
-  return Promise.allSettled(promises).then(results => {
-    return results.map(result => {
-      return {
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      };
-    });
-  });
+  return Promise.allSettled(promises).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
-
