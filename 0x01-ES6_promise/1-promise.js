7664257e73c6643a2c54ec3a8004a430dad91776
calvean <garnetsharara@gmail.com>
2023-04-19 17:08:55 +0200

    added curly bracket

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject({ error: 'The fake API is not working currently' });
+    }
+  });
+}
+
