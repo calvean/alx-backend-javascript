9a0c77d354a6102f3a92143fc8609bc0fdddcabc
calvean <garnetsharara@gmail.com>
2023-04-19 22:59:26 +0200

    babel error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-
+  let value;
   try {
-    const result = mathFunction();
-    queue.push(result);
+    value = mathFunction();
   } catch (error) {
-    queue.push(error.message);
+    value = error.toString();
   }
-
+  queue.push(value);
   queue.push('Guardrail was processed');
   return queue;
 }
