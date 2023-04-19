2ea5908a1f1ab92de2737a57247f359245016a54
calvean <garnetsharara@gmail.com>
2023-04-19 21:59:50 +0200

    added default

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
-      console.log(`${userResponse.firstName} ${userResponse.lastName} ${photoResponse.body}`);
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
