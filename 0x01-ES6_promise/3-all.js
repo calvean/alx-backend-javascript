c533d5d7979fe1323aeeb2a7aa907c23e4cf857d
calvean <garnetsharara@gmail.com>
2023-04-19 21:54:21 +0200

    added default

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(`${userResponse.firstName} ${userResponse.lastName} ${photoResponse.body}`);
