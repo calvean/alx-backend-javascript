e73ad8c9028137d8a8eb3340657ce3224f079a85
calvean <garnetsharara@gmail.com>
2023-04-20 17:04:52 +0200

    moved evac to constructor

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,15 @@
 export default class Building {
-  constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
+  constructor(sqft = 0) {
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+
     this._sqft = sqft;
   }
 
   // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  // evacuation message
-  evacuationWarningMessage() {
-    console.log(`The building has ${this._sqft} square feet.`);
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
