227136660b966c6fce6411ac4b8f8a27b4ecceee
calvean <garnetsharara@gmail.com>
2023-04-27 17:32:57 +0200

    advanced

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,12 +3,7 @@ export const weakMap = new WeakMap();
 export function queryAPI(endpoint) {
   if (weakMap.has(endpoint)) {
     const nQuery = weakMap.get(endpoint);
-    if (nQuery >= 5) {
-      throw new Error('Endpoint load is high');
-    } else {
-      weakMap.set(endpoint, nQuery + 1);
-    }
-  } else {
-    weakMap.set(endpoint, 1);
-  }
+    if (nQuery >= 4) throw Error('Endpoint load is high');
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else { weakMap.set(endpoint, 1); }
 }
