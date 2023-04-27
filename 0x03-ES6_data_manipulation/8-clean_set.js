75fd02817c575db6eb6f1fac3fe2985589c76bd0
calvean <garnetsharara@gmail.com>
2023-04-27 16:25:49 +0200

    task 7

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result += `${value.slice(startString.length)}-`;
+    }
+  }
+
+  return result.slice(0, -1);
+}
