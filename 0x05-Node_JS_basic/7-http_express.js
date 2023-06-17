16656b5c1af9ccd5cc1a0b0cb676f17f659e44ed
calvean <garnetsharara@gmail.com>
2023-06-17 15:20:36 +0200

    adds

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const students = await countStudents(process.argv[2]);
+    const response = `This is the list of our students\n${students.join('\n')}`;
+    res.send(response);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+module.exports = app;
