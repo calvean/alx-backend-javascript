d5a9962d2b34fa1a34ed0fd449ab4b6ea0b4674c
calvean <garnetsharara@gmail.com>
2023-06-17 16:11:26 +0200

    all

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,24 +1,24 @@
 import fs from 'fs';
 
-export const readDatabase = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (error, data) => {
-      if (error) {
-        reject(error);
-      } else {
-        const students = {
-          CS: [],
-          SWE: [],
-        };
-        const lines = data.trim().split('\n');
-        for (const line of lines) {
-          const [firstName, field] = line.split(',');
-          if (firstName && field) {
-            students[field.trim()].push(firstName.trim());
-          }
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (error, data) => {
+    if (error) {
+      reject(error);
+    } else {
+      const students = {
+        CS: [],
+        SWE: [],
+      };
+      const lines = data.trim().split('\n');
+      for (const line of lines) {
+        const [firstName, field] = line.split(',');
+        if (firstName && field) {
+          students[field.trim()].push(firstName.trim());
         }
-        resolve(students);
       }
-    });
+      resolve(students);
+    }
   });
-};
+});
+
+export default readDatabase;
