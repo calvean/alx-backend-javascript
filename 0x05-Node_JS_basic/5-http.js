e74d184712f0a4b5ec20f18468ca9a894c18dec7
calvean <garnetsharara@gmail.com>
2023-06-17 14:41:43 +0200

    changes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,43 +1,40 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
-  const { method, url } = req;
+const hostname = '127.0.0.1';
+const port = 1245;
 
-  if (method === 'GET') {
-    if (url === '/') {
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end('Hello Holberton School!');
-    } else if (url === '/students') {
-      countStudents('./database.csv')
-        .then(() => {
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.write('This is the list of our students\n');
-
-          countStudents('./database.csv')
-            .then((data) => {
-              res.end(data);
-            })
-            .catch((error) => {
-              res.end(error.message);
-            });
-        })
-        .catch((error) => {
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end(error.message);
-        });
+const app = http.createServer(async (req, res) => {
+  try {
+    if (req.method === 'GET') {
+      if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello Holberton School!');
+      } else if (req.url === '/students') {
+        const output = await countStudents('./database.csv');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`This is the list of our students\n${output}`);
+      } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not found');
+      }
     } else {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.end('Not found');
+      res.statusCode = 405;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Method Not Allowed');
     }
-  } else {
-    res.writeHead(405, { 'Content-Type': 'text/plain' });
-    res.end('Method Not Allowed');
+  } catch (error) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(error.message);
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
