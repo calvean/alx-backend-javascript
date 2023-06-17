7e52bbc646220b0fc9b24b491c2e698c4c123558
calvean <garnetsharara@gmail.com>
2023-06-17 15:22:49 +0200

    adds

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,42 +1,29 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const args = process.argv.slice(2);
-const DATABASE = args[0];
-
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  const { url } = req;
-
-  if (url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if (url === '/students') {
-    try {
-      const students = await countStudents(DATABASE);
-      const response = `This is the list of our students\n${students}`;
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(response);
-    } catch (error) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end(error.message);
-    }
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not found');
+  } else if (req.url === '/students') {
+    let dbInfo = 'This is the list of our students\n';
+    await countStudents(process.argv[2])
+      .then((msg) => {
+        dbInfo += msg;
+        res.end(dbInfo);
+      })
+      .catch((err) => {
+        dbInfo += err.message;
+        res.end(dbInfo);
+      });
   }
 });
 
-if (!module.parent) {
-  app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
-}
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
 
 module.exports = app;
