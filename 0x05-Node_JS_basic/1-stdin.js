8341960c80510e2d74afc26e1c2e9b56d612338a
calvean <garnetsharara@gmail.com>
2023-06-17 13:24:07 +0200

    fixed exit to close

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,6 +8,6 @@ process.stdin.on('readable', () => {
   }
 });
 
-process.stdin.on('exit', () => {
+process.stdin.on('close', () => {
   process.stdout.write('This important software is now closing\n');
 });
