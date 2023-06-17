8b81a7e1ec3a5ef9d0a032a5775a316069aaeb3d
calvean <garnetsharara@gmail.com>
2023-06-17 15:00:58 +0200

    changes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,40 +1,40 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const args = process.argv.slice(2);
+const DATABASE = args[0];
+
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  try {
-    if (req.method === 'GET') {
-      if (req.url === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello Holberton School!');
-      } else if (req.url === '/students') {
-        const output = await countStudents('./database.csv');
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end(`This is the list of our students\n${output}`);
-      } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Not found');
-      }
-    } else {
-      res.statusCode = 405;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('Method Not Allowed');
-    }
-  } catch (error) {
-    res.statusCode = 500;
+  const { url } = req;
+
+  if (url === '/') {
+    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(error.message);
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    try {
+      const students = await countStudents(DATABASE);
+      const response = `This is the list of our students\n${students}`;
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(response);
+    } catch (error) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(error.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
   }
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
