f268182317e0503b909213ef6609b779e62a9d70
calvean <garnetsharara@gmail.com>
2023-06-25 21:28:43 +0200

    added more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,35 +7,11 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-
-  it('it round the first argument', () => {
     assert.equal(calculateNumber(1.0, 0), 1);
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(1.7, 0), 2);
-  });
-
-  it('it round the second argument', () => {
     assert.equal(calculateNumber(0, 1.0), 1);
-    assert.equal(calculateNumber(0, 1.3), 1);
-    assert.equal(calculateNumber(0, 1.7), 2);
-  });
-
-  it('it should return the right number', () => {
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(0, 1.2), 1);
-    assert.equal(calculateNumber(1.3, 1.3), 2);
-    assert.equal(calculateNumber(1.7, 1.2), 3);
-    assert.equal(calculateNumber(1.3, 1.8), 3);
-    assert.equal(calculateNumber(1.6, 1.8), 4);
-  });
-
-  it('it should correct for negative numbers', () => {
     assert.strictEqual(calculateNumber(-1, 1), 0);
     assert.strictEqual(calculateNumber(1, -1), 0);
     assert.strictEqual(calculateNumber(-1, -1), -2);
-  });
-  it('it should check arguments', () => {
     assert.strictEqual(isNaN(calculateNumber(1)), true);
     assert.strictEqual(isNaN(calculateNumber()), true);
   });
