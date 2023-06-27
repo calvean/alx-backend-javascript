0cf0f7fc64b9183cb2705407dba9849ddcbeb076
calvean <garnetsharara@gmail.com>
2023-06-27 11:58:36 +0200

    chages

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,20 +2,42 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
-  it('should round the numbers and return the sum', () => {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.6, 1), 3);
-    assert.strictEqual(calculateNumber(1.5, 4), 6);
-    assert.strictEqual(calculateNumber(2, 1.8), 4);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.equal(calculateNumber(1.0, 0), 1);
-    assert.equal(calculateNumber(0, 1.0), 1);
-    assert.strictEqual(calculateNumber(-1, 1), 0);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(-1, -1), -2);
-    assert.strictEqual(isNaN(calculateNumber(1)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
+  it('should round the numbers and return their sum', () => {
+    assert.strictEqual(calculateNumber(1.0, 3), 4);
+    assert.strictEqual(calculateNumber(1.3, 3), 4);
+    assert.strictEqual(calculateNumber(1.7, 3), 5);
+  });
+
+  it('should handle negative numbers', () => {
+    assert.strictEqual(calculateNumber(-2.5, 3.8), 2);
+    assert.strictEqual(calculateNumber(-1.7, -3.2), -5);
+  });
+
+  it('should handle decimal numbers', () => {
+    assert.strictEqual(calculateNumber(1.1, 2.9), 4);
+    assert.strictEqual(calculateNumber(2.4, 4.6), 7);
+  });
+
+  it('should handle zero', () => {
+    assert.strictEqual(calculateNumber(0, 5), 5);
+    assert.strictEqual(calculateNumber(3, 0), 3);
+    assert.strictEqual(calculateNumber(0, 0), 0);
+  });
+
+  it('should use the rounded values of a and b', () => {
+    assert.strictEqual(calculateNumber(1.4, 2.8), 4);
+  });
+
+  it('should handle first rounded number', () => {
+    assert.strictEqual(calculateNumber(5, 2.5), 8);
+    assert.strictEqual(calculateNumber(10, 1.7), 12);
+    assert.strictEqual(calculateNumber(20, 0), 20);
+  });
+
+  it('should handle second rounded number', () => {
+    assert.strictEqual(calculateNumber(2.5, 5), 8);
+    assert.strictEqual(calculateNumber(1.7, 10), 12);
+    assert.strictEqual(calculateNumber(0, 20), 20);
   });
 });
+
