3ea5204c839d52fd960c8089e7945758c77d63e5
calvean <garnetsharara@gmail.com>
2023-06-27 15:22:11 +0200

    additions

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -5,154 +5,100 @@ describe('API Integration Test Suite', () => {
   const baseUrl = 'http://localhost:7865';
 
   describe('GET / - Index Page', () => {
-    it('should return status code 200', () => {
-      return new Promise((resolve, reject) => {
-        request.get(baseUrl, (error, response) => {
-          if (error) {
-            reject(error);
-          } else {
-            expect(response.statusCode).to.equal(200);
-            resolve();
-          }
-        });
+    it('should return status code 200', (done) => {
+      request.get(baseUrl, (error, response) => {
+        expect(response.statusCode).to.equal(200);
+        done();
       });
     });
 
-    it('should return the message "Welcome to the payment system"', () => {
-      return new Promise((resolve, reject) => {
-        request.get(baseUrl, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            expect(body).to.equal('Welcome to the payment system');
-            resolve();
-          }
-        });
+    it('should return the message "Welcome to the payment system"', (done) => {
+      request.get(baseUrl, (error, response, body) => {
+        expect(body).to.equal('Welcome to the payment system');
+        done();
       });
     });
   });
 
   describe('GET /cart/:id - Cart Page', () => {
-    it('should return status code 200 when :id is a number', () => {
+    it('should return status code 200 when :id is a number', (done) => {
       const id = 123;
-      return new Promise((resolve, reject) => {
-        request.get(`${baseUrl}/cart/${id}`, (error, response) => {
-          if (error) {
-            reject(error);
-          } else {
-            expect(response.statusCode).to.equal(200);
-            resolve();
-          }
-        });
+      request.get(`${baseUrl}/cart/${id}`, (error, response) => {
+        expect(response.statusCode).to.equal(200);
+        done();
       });
     });
 
-    it('should return "Payment methods for cart :id" when :id is a number', () => {
+    it('should return "Payment methods for cart :id" when :id is a number', (done) => {
       const id = 123;
-      return new Promise((resolve, reject) => {
-        request.get(`${baseUrl}/cart/${id}`, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            expect(body).to.equal(`Payment methods for cart ${id}`);
-            resolve();
-          }
-        });
+      request.get(`${baseUrl}/cart/${id}`, (error, response, body) => {
+        expect(body).to.equal(`Payment methods for cart ${id}`);
+        done();
       });
     });
 
-    it('should return status code 404 when :id is not a number', () => {
+    it('should return status code 404 when :id is not a number', (done) => {
       const id = 'abc';
-      return new Promise((resolve, reject) => {
-        request.get(`${baseUrl}/cart/${id}`, (error, response) => {
-          if (error) {
-            reject(error);
-          } else {
-            expect(response.statusCode).to.equal(404);
-            resolve();
-          }
-        });
+      request.get(`${baseUrl}/cart/${id}`, (error, response) => {
+        expect(response.statusCode).to.equal(404);
+        done();
       });
     });
   });
 
   describe('GET /available_payments - Available Payments Endpoint', () => {
-    it('should return status code 200', () => {
-      return new Promise((resolve, reject) => {
-        request.get(`${baseUrl}/available_payments`, (error, response) => {
-          if (error) {
-            reject(error);
-          } else {
-            expect(response.statusCode).to.equal(200);
-            resolve();
-          }
-        });
+    it('should return status code 200', (done) => {
+      request.get(`${baseUrl}/available_payments`, (error, response) => {
+        expect(response.statusCode).to.equal(200);
+        done();
       });
     });
 
-    it('should return an object with the correct structure', () => {
-      return new Promise((resolve, reject) => {
-        request.get(`${baseUrl}/available_payments`, (error, response, body) => {
-          if (error) {
-            reject(error);
-          } else {
-            const expected = {
-              payment_methods: {
-                credit_cards: true,
-                paypal: false
-              }
-            };
-            expect(JSON.parse(body)).to.deep.equal(expected);
-            resolve();
+    it('should return an object with the correct structure', (done) => {
+      request.get(`${baseUrl}/available_payments`, (error, response, body) => {
+        const expected = {
+          payment_methods: {
+            credit_cards: true,
+            paypal: false
           }
-        });
+        };
+        expect(JSON.parse(body)).to.deep.equal(expected);
+        done();
       });
     });
   });
 
   describe('POST /login - Login Endpoint', () => {
-    it('should return status code 200', () => {
+    it('should return status code 200', (done) => {
       const username = 'Betty';
       const requestBody = { userName: username };
 
-      return new Promise((resolve, reject) => {
-        request.post(
-          {
-            url: `${baseUrl}/login`,
-            json: requestBody
-          },
-          (error, response) => {
-            if (error) {
-              reject(error);
-            } else {
-              expect(response.statusCode).to.equal(200);
-              resolve();
-            }
-          }
-        );
-      });
+      request.post(
+        {
+          url: `${baseUrl}/login`,
+          json: requestBody
+        },
+        (error, response) => {
+          expect(response.statusCode).to.equal(200);
+          done();
+        }
+      );
     });
 
-    it('should return the message "Welcome :username"', () => {
+    it('should return the message "Welcome :username"', (done) => {
       const username = 'Betty';
       const requestBody = { userName: username };
 
-      return new Promise((resolve, reject) => {
-        request.post(
-          {
-            url: `${baseUrl}/login`,
-            json: requestBody
-          },
-          (error, response, body) => {
-            if (error) {
-              reject(error);
-            } else {
-              expect(body).to.equal(`Welcome ${username}`);
-              resolve();
-            }
-          }
-        );
-      });
+      request.post(
+        {
+          url: `${baseUrl}/login`,
+          json: requestBody
+        },
+        (error, response, body) => {
+          expect(body).to.equal(`Welcome ${username}`);
+          done();
+        }
+      );
     });
   });
 });
