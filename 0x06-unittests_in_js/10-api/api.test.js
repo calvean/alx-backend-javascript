84cdb0cd57a98fca65faff5c8ccd81341116fe88
calvean <garnetsharara@gmail.com>
2023-06-27 15:33:13 +0200

    additions

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,104 +1,63 @@
 const request = require('request');
-const { expect } = require('chai');
+const chai = require('chai');
+
+const baseUrl = 'http://localhost:7865';
 
 describe('API Integration Test Suite', () => {
-  const baseUrl = 'http://localhost:7865';
-
-  describe('GET / - Index Page', () => {
-    it('should return status code 200', (done) => {
-      request.get(baseUrl, (error, response) => {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-
-    it('should return the message "Welcome to the payment system"', (done) => {
+  describe('GET /', () => {
+    it('should return status code 200 and correct message', (done) => {
       request.get(baseUrl, (error, response, body) => {
-        expect(body).to.equal('Welcome to the payment system');
+        chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.equal('Welcome to the payment system');
         done();
       });
     });
   });
 
-  describe('GET /cart/:id - Cart Page', () => {
-    it('should return status code 200 when :id is a number', (done) => {
-      const id = 123;
-      request.get(`${baseUrl}/cart/${id}`, (error, response) => {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-
-    it('should return "Payment methods for cart :id" when :id is a number', (done) => {
-      const id = 123;
+  describe('GET /cart/:id', () => {
+    it('should return status code 200 and correct message when :id is a number', (done) => {
+      const id = 12;
       request.get(`${baseUrl}/cart/${id}`, (error, response, body) => {
-        expect(body).to.equal(`Payment methods for cart ${id}`);
+        chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.equal(`Payment methods for cart ${id}`);
         done();
       });
     });
 
     it('should return status code 404 when :id is not a number', (done) => {
-      const id = 'abc';
-      request.get(`${baseUrl}/cart/${id}`, (error, response) => {
-        expect(response.statusCode).to.equal(404);
+      const id = 'anything';
+      request.get(`${baseUrl}/cart/${id}`, (error, response, body) => {
+        chai.expect(response.statusCode).to.equal(404);
         done();
       });
     });
   });
 
-  describe('GET /available_payments - Available Payments Endpoint', () => {
-    it('should return status code 200', (done) => {
-      request.get(`${baseUrl}/available_payments`, (error, response) => {
-        expect(response.statusCode).to.equal(200);
-        done();
-      });
-    });
-
-    it('should return an object with the correct structure', (done) => {
+  describe('GET /available_payments', () => {
+    it('should return status code 200 and correct payment methods', (done) => {
       request.get(`${baseUrl}/available_payments`, (error, response, body) => {
-        const expected = {
-          payment_methods: {
-            credit_cards: true,
-            paypal: false
-          }
-        };
-        expect(JSON.parse(body)).to.deep.equal(expected);
+        chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.equal('{"payment_methods":{"credit_cards":true,"paypal":false}}');
         done();
       });
     });
   });
 
-  describe('POST /login - Login Endpoint', () => {
-    it('should return status code 200', (done) => {
+  describe('POST /login', () => {
+    it('should return status code 200 and welcome message with username', (done) => {
       const username = 'Betty';
-      const requestBody = { userName: username };
-
       request.post(
         {
           url: `${baseUrl}/login`,
-          json: requestBody
-        },
-        (error, response) => {
-          expect(response.statusCode).to.equal(200);
-          done();
-        }
-      );
-    });
-
-    it('should return the message "Welcome :username"', (done) => {
-      const username = 'Betty';
-      const requestBody = { userName: username };
-
-      request.post(
-        {
-          url: `${baseUrl}/login`,
-          json: requestBody
+          json: { userName: username },
         },
         (error, response, body) => {
-          expect(body).to.equal(`Welcome ${username}`);
+          chai.expect(response.statusCode).to.equal(200);
+          chai.expect(body).to.equal(`Welcome ${username}`);
           done();
         }
       );
     });
   });
 });
+
