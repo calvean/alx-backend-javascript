8781a38b220c7ab34fde1d17b1bc2cac64381387
calvean <garnetsharara@gmail.com>
2023-06-27 15:35:42 +0200

    additions

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -2,34 +2,20 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-});
-
-app.get('/cart/:id', (req, res) => {
-  const { id } = req.params;
-  if (isNaN(id)) {
-    res.status(404).send('Invalid cart ID');
-  } else {
-    res.send(`Payment methods for cart ${id}`);
-  }
-});
-
+app.get('/', (req, res) => res.end('Welcome to the payment system'))
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.end(`Payment methods for cart ${req.params.id}`)
+})
 app.get('/available_payments', (req, res) => {
-  const paymentMethods = {
+  const obj = {
     payment_methods: {
       credit_cards: true,
       paypal: false,
     },
   };
-  res.json(paymentMethods);
+  res.json(obj);
 });
-
 app.post('/login', (req, res) => {
-  const { userName } = req.body;
-  res.send(`Welcome ${userName}`);
+  res.end(`Welcome ${req.body.userName}`);
 });
-
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
+app.listen(port, () => console.log(`API available on localhost port ${port}`));
