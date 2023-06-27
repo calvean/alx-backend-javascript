40e50437c7e98c7a0400f5a94f33225601d30168
calvean <garnetsharara@gmail.com>
2023-06-27 15:26:08 +0200

    additions

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -2,24 +2,34 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'));
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  const cartId = req.params.id;
-  res.end(`Payment methods for cart ${cartId}`);
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+  if (isNaN(id)) {
+    res.status(404).send('Invalid cart ID');
+  } else {
+    res.send(`Payment methods for cart ${id}`);
+  }
 });
 
 app.get('/available_payments', (req, res) => {
   const paymentMethods = {
-    credit_cards: true,
-    paypal: false
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
   };
-  res.json({ payment_methods: paymentMethods });
+  res.json(paymentMethods);
 });
 
 app.post('/login', (req, res) => {
-  const username = req.body.userName;
-  res.end(`Welcome ${username}`);
+  const { userName } = req.body;
+  res.send(`Welcome ${userName}`);
 });
 
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
