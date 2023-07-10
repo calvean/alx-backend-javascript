571ef423287d9ce9ad769ca5ffd6955a74c01ecc
calvean <garnetsharara@gmail.com>
2023-07-10 15:39:12 +0200

    additions

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -59,50 +59,52 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
   }
 });
 
+const handleRoute = (route, handler) => ({
+  route,
+  handler: (req, res) => {
+    const responseParts = ['This is the list of our students'];
+
+    handler(DB_FILE)
+      .then((report) => {
+        responseParts.push(report);
+        const responseText = responseParts.join('\n');
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Length', responseText.length);
+        res.statusCode = 200;
+        res.write(Buffer.from(responseText));
+      })
+      .catch((err) => {
+        responseParts.push(err instanceof Error ? err.message : err.toString());
+        const responseText = responseParts.join('\n');
+        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Length', responseText.length);
+        res.statusCode = 200;
+        res.write(Buffer.from(responseText));
+      });
+  },
+});
+
 const SERVER_ROUTE_HANDLERS = [
-  {
-    route: '/',
-    handler(_, res) {
-      const responseText = 'Hello Holberton School!';
-
-      res.setHeader('Content-Type', 'text/plain');
-      res.setHeader('Content-Length', responseText.length);
-      res.statusCode = 200;
-      res.write(Buffer.from(responseText));
-    },
-  },
-  {
-    route: '/students',
-    handler(_, res) {
-      const responseParts = ['This is the list of our students'];
-
-      countStudents(DB_FILE)
-        .then((report) => {
-          responseParts.push(report);
-          const responseText = responseParts.join('\n');
-          res.setHeader('Content-Type', 'text/plain');
-          res.setHeader('Content-Length', responseText.length);
-          res.statusCode = 200;
-          res.write(Buffer.from(responseText));
-        })
-        .catch((err) => {
-          responseParts.push(err instanceof Error ? err.message : err.toString());
-          const responseText = responseParts.join('\n');
-          res.setHeader('Content-Type', 'text/plain');
-          res.setHeader('Content-Length', responseText.length);
-          res.statusCode = 200;
-          res.write(Buffer.from(responseText));
-        });
-    },
-  },
+  handleRoute('/', (_, res) => {
+    const responseText = 'Hello Holberton School!';
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', responseText.length);
+    res.statusCode = 200;
+    res.write(Buffer.from(responseText));
+  }),
+  handleRoute('/students', countStudents),
 ];
 
 app.on('request', (req, res) => {
-  for (const routeHandler of SERVER_ROUTE_HANDLERS) {
-    if (routeHandler.route === req.url) {
-      routeHandler.handler(req, res);
-      break;
-    }
+  const routeHandler = SERVER_ROUTE_HANDLERS.find(
+    (handler) => handler.route === req.url,
+  );
+  if (routeHandler) {
+    routeHandler.handler(req, res);
+  } else {
+    res.setHeader('Content-Type', 'text/plain');
+    res.statusCode = 404;
+    res.end('404 Not Found');
   }
 });
 
