abf3a31c7a28d4167ddfe4f0db9f620d9d3a7416
calvean <garnetsharara@gmail.com>
2023-10-13 20:22:43 +0200

    changes

--- 0x06-unittests_in_js/0-calcul-2.js
+++
@@ -1,5 +0,0 @@
-function calculateNumber(a, b) {
-    return Math.ceil(a) + Math.ceil(b);
-}
-  
-module.exports = calculateNumber;
--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,9 +0,0 @@
-function getPaymentTokenFromAPI(success) {
-  if (success) {
-    return Promise.resolve({ data: 'Successful response from the API' });
-  } else {
-    return Promise.resolve();
-  }
-}
-
-module.exports = getPaymentTokenFromAPI;
